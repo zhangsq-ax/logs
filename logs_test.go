afe5ef16118f5d3c20cf71e5a21fd8550daad886
张世琦 <zhangsq@zhangshiqideMacBook-Pro.local>
2024-06-26 11:21:14 +0800

    fix: 增加实时改变日志级别功能 StartMonitorLogLevel()

--- logs_test.go
+++ logs_test.go
@@ -1,16 +1,14 @@
 package logs
 
 import (
-	"context"
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/zap"
+	"fmt"
 	"go.uber.org/zap/zapcore"
 	"os"
 	"testing"
 	"time"
 )
 
-func TestConfigEnvLogLevels(t *testing.T) {
+/*func TestConfigEnvLogLevels(t *testing.T) {
 	// 默认
 	level := GetLogLevel()
 	assert.Equal(t, zapcore.InfoLevel, level)
@@ -64,4 +62,50 @@ func TestSetLogFile(t *testing.T) {
 			logger.Infow("test", zap.String("foo", "bar"))
 		}
 	}
+}*/
+
+func TestLogger(t *testing.T) {
+	go func() {
+		for {
+			fmt.Println("--------------------------")
+			Logger().Debug("this is debug")
+			Logger().Info("this is info")
+			Logger().Warn("this is warn")
+			Logger().Error("this is error")
+			fmt.Println("===========================")
+			time.Sleep(5 * time.Second)
+		}
+	}()
+}
+
+func TestStartMonitorLogLevel(t *testing.T) {
+	StartMonitorLogLevel(5 * time.Second)
+	time.Sleep(10 * time.Second)
+	t.Log("set dev")
+	_ = os.Setenv("ENV", "dev")
+	time.Sleep(10 * time.Second)
+	t.Log("set test")
+	_ = os.Setenv("ENV", "test")
+	time.Sleep(10 * time.Second)
+	t.Log("set prod")
+	_ = os.Setenv("ENV", "prod")
+	time.Sleep(10 * time.Second)
+	t.Log("unset")
+	_ = os.Unsetenv("ENV")
+	time.Sleep(20 * time.Second)
+}
+
+func TestSetDefaultLogLevel(t *testing.T) {
+	t.Log("set debug level")
+	SetDefaultLogLevel(zapcore.DebugLevel)
+	time.Sleep(10 * time.Second)
+	t.Log("set info level")
+	SetDefaultLogLevel(zapcore.InfoLevel)
+	time.Sleep(10 * time.Second)
+	t.Log("set warn level")
+	SetDefaultLogLevel(zapcore.WarnLevel)
+	time.Sleep(10 * time.Second)
+	t.Log("set error level")
+	SetDefaultLogLevel(zapcore.ErrorLevel)
+	time.Sleep(20 * time.Second)
 }
